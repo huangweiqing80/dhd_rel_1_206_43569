e79e12654c6f940def2fce9541b60c403e6e03c8
Frank Yang <frank2060@gmail.com>
2015-08-03 11:39:33 +0800

    logF5: To fix bcmdl build script for ubuntu

--- bfd-app-bcmdl-x86.sh
+++ bfd-app-bcmdl-x86.sh
@@ -3,7 +3,12 @@
 CUR_DIR=`pwd`
 export LIBUSB=${CUR_DIR}/src/usbdev/libusb-1.0.9
 export LIBUSB_COMPACT=${CUR_DIR}/src/usbdev/libusb-compat-0.1.3
+if [ "${TARGETDIR}"="" ]; then
+export TARGETDIR=`uname -r`
 mkdir ${TARGETDIR}
+else
+mkdir ${TARGETDIR}
+fi
 cd ${CUR_DIR}/src/usbdev
 tar xvf libusb-1.0.9.tar.bz2
 tar xvf libusb-compat-0.1.3.tar.bz2
